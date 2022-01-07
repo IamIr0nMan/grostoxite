ac683f193eacd035cb5639e4bfb7265b3257c31a
Deepak Kumar <iamdpk18@gmail.com>
2022-01-07 15:33:23 +0530

    Add funds component

---
+++ src/components/funds.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Funds() {
+  return (
+    <div className="split right">
+      <div className="m-5">
+        <div className="d-flex flex-row-reverse">
+          <button type="button" class="btn btn-lg btn-primary m-2">
+            Withdraw
+          </button>
+          <button type="button" class="btn btn-lg btn-success m-2">
+            Add Funds
+          </button>
+        </div>
+        <table className="table table-hover">
+          <tbody>
+            <tr>
+              <td>
+                <div>Available Balance in Trading Account</div>
+              </td>
+              <td>
+                <h2>0.00</h2>
+              </td>
+            </tr>
+            <tr>
+              <td>Bank Balance</td>
+              <td>
+                <h2>0.00</h2>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Funds;
