2e200941d723bd066024c1a7d4b361f178ba7b5b
Deepak Kumar <iamdpk18@gmail.com>
2022-01-25 17:12:09 +0530

    Add transaction schema model for the database collection

---
+++ models/transactionSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema({
+  investor: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  asset: { type: mongoose.Types.ObjectId, required: true, refPath: "onModel" },
+  date: { type: Date, required: true },
+  units: { type: mongoose.Types.Decimal128, required: true },
+  status: { type: Boolean, required: true },
+  amountInvested: { type: mongoose.Types.Decimal128, required: true },
+  onModel: {
+    type: String,
+    required: true,
+    enum: ["StockList", "MutualFundList"],
+  },
+});
+
+module.exports = mongoose.model("Transaction", transactionSchema);
