580c1d453e318fce3babb324fee20b159cb020bd
Deepak Kumar <iamdpk18@gmail.com>
2022-01-27 21:02:14 +0530

    Add database controller functions for signup and login

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,11 +1,99 @@
-const signup = (req, res, next) => {
-  console.log("User registration complete...");
-  res.json({ message: "Hello User, welcome to our website..." });
+const HttpError = require("../models/http-error");
+const Portfolio = require("../models/portfolioSchema");
+const User = require("../models/userSchema");
+
+const signup = async (req, res, next) => {
+  const {
+    fname,
+    lname,
+    email,
+    password,
+    dob,
+    identification,
+    mobile,
+    address,
+    bankName,
+    accountNumber,
+    tradingBalance,
+    bankBalance,
+  } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (error) {
+    console.error("Checking existing user with email on signup failed\n");
+    console.log(error);
+    err = new HttpError("Signing up failed, please try again later", 500);
+    return next(err);
+  }
+
+  if (existingUser) {
+    err = new HttpError("User already exists, please login instead", 422);
+    return next(err);
+  }
+
+  const createdUser = new User({
+    fname,
+    lname,
+    email,
+    password,
+    dob: new Date(dob),
+    identification,
+    mobile,
+    address,
+    bankName,
+    accountNumber,
+    tradingBalance,
+    bankBalance,
+  });
+
+  try {
+    await createdUser.save().then(async (createdUser) => {
+      const userPortfolio = new Portfolio({
+        investor: createdUser._id,
+      });
+      try {
+        await userPortfolio.save();
+      } catch (errObj) {
+        console.error("Creating and saving user portfolio to database failed");
+        console.log(errObj);
+      }
+    });
+  } catch (error) {
+    console.error("Saving new user to database failed");
+    console.log(error);
+    err = new HttpError("Signing up failed, please try again later", 500);
+    return next(err);
+  }
+
+  res
+    .status(201)
+    .json({ message: "User signup successful", user: createdUser });
 };
 
-const login = (req, res, next) => {
-  console.log("User logged in...");
-  res.json({ message: "User is logged in to the website..." });
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (error) {
+    console.error("Checking existing user on login failed");
+    console.log(error);
+    const err = new HttpError("Login failed, please try again", 500);
+    return next(err);
+  }
+
+  if (!existingUser) {
+    const error = new HttpError("User does not exist, please sign up", 401);
+    return next(error);
+  } else if (existingUser.password !== password) {
+    const error = new HttpError("Invalid Credentials, please try again", 401);
+    return next(error);
+  }
+
+  res.status(200).json({ message: "User logged in...", id: existingUser._id });
 };
 
 const updatePassword = (req, res, next) => {
