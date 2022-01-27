99744dfba4daad9ab747e362bfd99645a520c581
Deepak Kumar <iamdpk18@gmail.com>
2022-01-27 14:34:40 +0530

    Add StockList and MutualFundList schema

---
+++ models/mutualFundListSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const mongooseUniqueValidator = require("mongoose-unique-validator");
+
+mongoose.pluralize(null);
+
+const Schema = mongoose.Schema;
+
+const mutualFundsSchema = new Schema({
+  fundName: String,
+  amcName: String,
+  sector: String,
+  fundManager: String,
+  aum: String,
+  expenseRatio: String,
+  inceptionDate: Date,
+  nav: [{ date: Date, nav: mongoose.Types.Decimal128 }],
+});
+
+mutualFundsSchema.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.Model("MutualFundList", mutualFundsSchema);
---
+++ models/stockListSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const mongooseUniqueValidator = require("mongoose-unique-validator");
+
+mongoose.pluralize(null);
+
+const Schema = mongoose.Schema;
+
+const stockListSchema = new Schema({
+  ticker: { type: String, required: true, unique: true },
+  companyName: String,
+  website: String,
+  "CEO/Founder": String,
+  sector: String,
+  businessSummary: String,
+  fundamentals: {
+    PE: String,
+    marketCap: String,
+    dividendYield: String,
+    fiftyTwoWeekHigh: String,
+    fiftyTwoWeekLow: String,
+    ebitda: String,
+    totalDebt: String,
+    debtToEquity: String,
+    returnOnEquity: String,
+    grossProfits: String,
+  },
+  price: [
+    {
+      date: Date,
+      open: mongoose.Types.Decimal128,
+      dayLow: mongoose.Types.Decimal128,
+      dayHigh: mongoose.Types.Decimal128,
+      close: mongoose.Types.Decimal128,
+    },
+  ],
+});
+
+stockListSchema.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.Model("StockList", stockListSchema);
