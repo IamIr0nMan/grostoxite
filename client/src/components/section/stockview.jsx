24cc04017976227fbbb03ec2df415607b08c7941
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2022-01-30 16:14:00 +0530

    added the state change prop for the stockview and added the animation for that part

--- client/src/components/section/stockview.jsx
+++ client/src/components/section/stockview.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-export default function StockView() {
+export default function StockView(props) {
+  // console.log(data);
   return (
     <div>
-      <ul className="ayush-trying">
-        <li className="ayush-left-li">Ayush</li>
-        <li className="ayush-right-li">18.4 &uarr;</li>
-        <li className="ayush-right-li">12457</li>
-      </ul>
-      <hr />
+    <div className="stockview-anime" >
+        <ul className="ayush-trying">
+          {/* {/* <li className="ayush-left-li">Ayush</li> */}
+          <li className="ayush-left-li "><a href="www.google.com" className="stockview-a">{props.name}</a></li>
+        {/* <li className="ayush-right-li">18.4 &uarr;</li> */}
+          <li className="ayush-right-li">{props.height} &uarr;</li>
+          {/* <li className="ayush-right-li">12457</li> */}
+          <li className="ayush-right-li">{props.mass}</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- client/src/components/section/watchlist.jsx
+++ client/src/components/section/watchlist.jsx
@@ -2,15 +2,36 @@ import React from "react";
 import StockView from "./stockview.jsx";
 
 export default function WatchList() {
+  
+  const [myState, setmyState] = React.useState([]);
+  React.useEffect(() => {
+    fetch('https://swapi.dev/api/people/').
+    then(res => res.json()).
+    then(data => {
+      setmyState(data.results)}).catch(err => console.log(err));
+    }, [])
+//  console.log(myState);
+
+const jokeElements = myState.map((data)=> {
+  return (
+      <StockView 
+          // name = {data.name}
+          name = {data.name}
+          height = {data.height}
+          mass = {data.mass}
+      / >)
+})
+
   return (
     <div>
       <div className="split left">
         <div /* className="centered" */>
+        {/* <pre>{JSON.stringify(myState, null, 2)}</pre> */}
+           {/* <StockView />
           <StockView />
           <StockView />
-          <StockView />
-          <StockView />
-          <StockView />
+          <StockView />  */}
+          {jokeElements}
         </div>
       </div>
     </div>
